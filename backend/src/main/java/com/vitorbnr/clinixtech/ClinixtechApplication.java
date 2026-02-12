4216a6da8cdff753005519b9a8b86799a7ad80dd
vitorbnr <vitorbernardotech@gmail.com>
2026-02-11 21:11:21 -0300

    chore: configuração inicial do projeto com docker-compose e configuração de ambiente

---
+++ backend/src/main/java/com/vitorbnr/clinixtech/ClinixtechApplication.java
@@ -0,0 +1,13 @@
+package com.vitorbnr.clinixtech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinixtechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinixtechApplication.class, args);
+	}
+
+}
