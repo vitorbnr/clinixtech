c8a958f77db84f378f6b3f4d5eaa036dc8679f59
vitorbnr <vitorbernardotech@gmail.com>
2026-02-12 22:20:27 -0300

    fix: loop no login

---
+++ backend/src/main/java/com/vitorbnr/clinixtech/service/AuthorizationService.java
@@ -0,0 +1,21 @@
+package com.vitorbnr.clinixtech.service;
+
+import com.vitorbnr.clinixtech.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthorizationService implements UserDetailsService {
+
+    @Autowired
+    UserRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return repository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado com este email"));
+    }
+}
